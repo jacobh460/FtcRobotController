9133ccd8aa120402e8f68c5f47057bde37927e3c
jacob <jacobhillman016@gmail.com>
2023-09-30 16:57:03 -0700

    created a test file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_motor.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="test_drive", group="Linear OpMode")
+
+public class test_motor extends LinearOpMode {
+
+    DcMotorEx motor0 = null;
+
+    public void runOpMode(){
+        motor0 = this.hardwareMap.get(DcMotorEx.class, "motor0");
+
+        waitForStart();
+
+        motor0.setPower(0);
+
+        while (this.opModeIsActive()) {
+            motor0.setPower(gamepad1.left_stick_y);
+        }
+
+        motor0.setPower(0);
+
+    }
+
+
+}
