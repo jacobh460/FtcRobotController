c86bf2d514193ee45b6b3e24c0df351b60c65b2c
jacobh460 <jacobhillman016@gmail.com>
2023-11-11 17:16:28 -0800

    tested hand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servoTest.java
@@ -1,11 +1,25 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
+
+@TeleOp
 public class servoTest extends LinearOpMode {
 
     @Override
     public void runOpMode(){
+        Servo servo = hardwareMap.get(Servo.class, "testServo");
 
+        waitForStart();
+
+        servo.setPosition(0.5);
+
+        while (opModeIsActive()){
+            sleep(5);
+        }
+
+        return;
     }
 }
